42212c146fe689614ed76744b24f1ace892698a9
Matheus Sousa <matheusss.dev01@gmail.com>
2025-08-11 21:36:58 -0300

    Exercício 01 e 02 resolvido

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/exercice-html', response_class=HTMLResponse)
+def exercice_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
